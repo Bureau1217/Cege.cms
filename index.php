e9bcf004380de4c28e6c895a42b633552a81f26b
gabrielabergel <44164440+gabrielabergel@users.noreply.github.com>
2026-02-05 14:31:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,23 @@ require __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
+// CORS - Allow frontend
+$allowedOrigins = ['https://cegeswiss.com'];
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
+if (in_array($origin, $allowedOrigins)) {
+    header("Access-Control-Allow-Origin: $origin");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Access-Control-Max-Age: 86400");
+}
+
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 require 'kirby/bootstrap.php';
 
 echo (new Kirby)->render();
